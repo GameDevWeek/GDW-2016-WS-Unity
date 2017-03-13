10628b4bd7a9e85d662a3683d0ada46e0e77748d
Armin Willer <armin_willer@yahoo.de>
2017-03-13 15:23:44 +0100

    Fixed rendering bugs

--- Assets/Scripts/Rendering/HideOutOfPlayerRange.cs
+++ Assets/Scripts/Rendering/HideOutOfPlayerRange.cs
@@ -29,15 +29,18 @@ public class HideOutOfPlayerRange : MonoBehaviour
         Vector3 directionToPlayer = vectorToPlayer.normalized;
         Vector3 startPosition = transform.position + directionToPlayer * objectRadius;
         Vector3 orthogonalVector = new Vector3(directionToPlayer.z, 0, -directionToPlayer.x);
+        Vector3 leftRayDirection = (playerPos - (startPosition + orthogonalVector * raycastOriginOffset)).normalized;
+        Vector3 rightRayDirection = (playerPos - (startPosition - orthogonalVector * raycastOriginOffset)).normalized;
         RaycastHit hit1;
         RaycastHit hit2;
 
         bool isInDistance = vectorToPlayer.magnitude < visibleDistance;
-        bool hitBoth = Physics.Raycast(startPosition + orthogonalVector * raycastOriginOffset, directionToPlayer, out hit1, vectorToPlayer.magnitude, viewBlockingLayers);
-        hitBoth &= Physics.Raycast(startPosition - orthogonalVector * raycastOriginOffset, directionToPlayer, out hit2, vectorToPlayer.magnitude, viewBlockingLayers);
-        Debug.DrawLine(startPosition + orthogonalVector * raycastOriginOffset, startPosition + orthogonalVector * raycastOriginOffset + directionToPlayer * vectorToPlayer.magnitude);
-        Debug.DrawLine(startPosition - orthogonalVector * raycastOriginOffset, startPosition - orthogonalVector * raycastOriginOffset + directionToPlayer * vectorToPlayer.magnitude);
-        Debug.DrawLine(startPosition, playerPos, Color.red);
+        bool hitBoth = Physics.Raycast(startPosition + orthogonalVector * raycastOriginOffset, leftRayDirection, out hit1, vectorToPlayer.magnitude, viewBlockingLayers);
+        hitBoth &= Physics.Raycast(startPosition - orthogonalVector * raycastOriginOffset, rightRayDirection, out hit2, vectorToPlayer.magnitude, viewBlockingLayers);
+        if (hit1.collider == null) Debug.DrawLine(startPosition + orthogonalVector * raycastOriginOffset, startPosition + orthogonalVector * raycastOriginOffset + leftRayDirection * vectorToPlayer.magnitude);
+        else Debug.DrawLine(startPosition + orthogonalVector * raycastOriginOffset, hit1.point, Color.cyan);
+        if (hit2.collider == null) Debug.DrawLine(startPosition - orthogonalVector * raycastOriginOffset, startPosition - orthogonalVector * raycastOriginOffset + rightRayDirection * vectorToPlayer.magnitude);
+        else Debug.DrawLine(startPosition - orthogonalVector * raycastOriginOffset, hit2.point, Color.cyan);
         bool visible = isInDistance && !hitBoth;
         for(int i = 0; i < renderers.Length; ++i)
         {
