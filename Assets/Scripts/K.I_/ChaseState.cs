1fed344a6dd2cbecd0467c1193f723fe7b994052
Andreas Hergert <hergerta@hochschule-trier.de>
2017-03-13 17:34:06 +0100

    Angepasste Scripts

--- Assets/Scripts/K.I_/ChaseState.cs
+++ Assets/Scripts/K.I_/ChaseState.cs
@@ -39,39 +39,39 @@ public class ChaseState : IEnemyState
 
     private void Look()
     {
-        Vector3[] boundPoints = new Vector3[3];
-        RaycastHit boundsHit, boundsHit1, boundsHit2;
+        //Vector3[] boundPoints = new Vector3[3];
+        //RaycastHit boundsHit, boundsHit1, boundsHit2;
 
-        var targetPoint = PlayerActor.Instance.collider.bounds.ClosestPoint(enemy.eyes.transform.position);
-        var minPoint = PlayerActor.Instance.collider.bounds.min;
-        var maxPoint = PlayerActor.Instance.collider.bounds.max;
+        //var targetPoint = PlayerActor.Instance.collider.bounds.ClosestPoint(enemy.eyes.transform.position);
+        //var minPoint = PlayerActor.Instance.collider.bounds.min;
+        //var maxPoint = PlayerActor.Instance.collider.bounds.max;
 
-        boundPoints[0] = targetPoint;
-        boundPoints[1] = minPoint;
-        boundPoints[2] = maxPoint;
+        //boundPoints[0] = targetPoint;
+        //boundPoints[1] = minPoint;
+        //boundPoints[2] = maxPoint;
 
-        if (Physics.Raycast(enemy.eyes.transform.position, boundPoints[0] - enemy.eyes.transform.position, out boundsHit, enemy.sightRange) &&
-            Physics.Raycast(enemy.eyes.transform.position, boundPoints[1] - enemy.eyes.transform.position, out boundsHit1, enemy.sightRange) &&
-            Physics.Raycast(enemy.eyes.transform.position, boundPoints[2] - enemy.eyes.transform.position, out boundsHit2, enemy.sightRange))
-            if (boundsHit.collider.CompareTag("Player"))
-            {
-                enemy.chaseTarget = boundsHit.transform;
-            }
-        else
-            {
-                ToAlertState();
-            }
-
-        //RaycastHit hit;
-        //Vector3 enemyToTarget = (enemy.chaseTarget.position + enemy.offset) - enemy.eyes.transform.position;
-        //if (Physics.Raycast(enemy.eyes.transform.position, enemyToTarget, out hit, enemy.sightRange) && hit.collider.CompareTag("Player"))
-        //{
-        //    enemy.chaseTarget = hit.transform;
-        //}
+        //if (Physics.Raycast(enemy.eyes.transform.position, boundPoints[0] - enemy.eyes.transform.position, out boundsHit, enemy.sightRange) &&
+        //    Physics.Raycast(enemy.eyes.transform.position, boundPoints[1] - enemy.eyes.transform.position, out boundsHit1, enemy.sightRange) &&
+        //    Physics.Raycast(enemy.eyes.transform.position, boundPoints[2] - enemy.eyes.transform.position, out boundsHit2, enemy.sightRange))
+        //    if (boundsHit.collider.CompareTag("Player"))
+        //    {
+        //        enemy.chaseTarget = boundsHit.transform;
+        //    }
         //else
-        //{
-        //    ToAlertState();
-        //}
+        //    {
+        //        ToAlertState();
+        //    }
+
+        RaycastHit hit;
+        Vector3 enemyToTarget = (enemy.chaseTarget.position + enemy.offset) - enemy.eyes.transform.position;
+        if (Physics.Raycast(enemy.eyes.transform.position, enemyToTarget, out hit, enemy.sightRange) && hit.collider.CompareTag("Player"))
+        {
+            enemy.chaseTarget = hit.transform;
+        }
+        else
+        {
+            ToAlertState();
+        }
     }
 
     private void Chase()
--- Assets/Scripts/K.I_/PatrolState.cs
+++ Assets/Scripts/K.I_/PatrolState.cs
@@ -42,39 +42,39 @@ public class PatrolState : IEnemyState
     private void Look()
     {
         //Bounds test -------------------------------------
-        Vector3[] boundPoints = new Vector3[3];
-        RaycastHit boundsHit, boundsHit1, boundsHit2;
+        //Vector3[] boundPoints = new Vector3[3];
+        //RaycastHit boundsHit, boundsHit1, boundsHit2;
 
-        var targetPoint = PlayerActor.Instance.collider.bounds.ClosestPoint(enemy.eyes.transform.position);
-        var minPoint = PlayerActor.Instance.collider.bounds.min;
-        var maxPoint = PlayerActor.Instance.collider.bounds.max;
+        //var targetPoint = PlayerActor.Instance.collider.bounds.ClosestPoint(enemy.eyes.transform.position);
+        //var minPoint = PlayerActor.Instance.collider.bounds.min;
+        //var maxPoint = PlayerActor.Instance.collider.bounds.max;
 
-        boundPoints[0] = targetPoint;
-        boundPoints[1] = minPoint;
-        boundPoints[2] = maxPoint;
+        //boundPoints[0] = targetPoint;
+        //boundPoints[1] = minPoint;
+        //boundPoints[2] = maxPoint;
 
-        int oldlayer = enemy.gameObject.layer;
-        enemy.gameObject.layer = GameLayer.IgnoreRaycast;
-        if (Physics.Raycast(enemy.eyes.transform.position, boundPoints[0] - enemy.eyes.transform.position, out boundsHit, enemy.sightRange) &&
-            Physics.Raycast(enemy.eyes.transform.position, boundPoints[1] - enemy.eyes.transform.position, out boundsHit1, enemy.sightRange) &&
-            Physics.Raycast(enemy.eyes.transform.position, boundPoints[2] - enemy.eyes.transform.position, out boundsHit2, enemy.sightRange))
-            //Debug.Log(boundsHit.transform.gameObject);
-            if (boundsHit.collider.CompareTag("Player") == PlayerActor.Instance.transform)
-            {
-                enemy.chaseTarget = boundsHit.transform;
-                ToChaseState();
-            }
-        enemy.gameObject.layer = oldlayer;
-    
+        //int oldlayer = enemy.gameObject.layer;
+        //enemy.gameObject.layer = GameLayer.IgnoreRaycast;
+        //if (Physics.Raycast(enemy.eyes.transform.position, boundPoints[0] - enemy.eyes.transform.position, out boundsHit, enemy.sightRange) &&
+        //    Physics.Raycast(enemy.eyes.transform.position, boundPoints[1] - enemy.eyes.transform.position, out boundsHit1, enemy.sightRange) &&
+        //    Physics.Raycast(enemy.eyes.transform.position, boundPoints[2] - enemy.eyes.transform.position, out boundsHit2, enemy.sightRange))
+        //    //Debug.Log(boundsHit.transform.gameObject);
+        //    if (boundsHit.collider.CompareTag("Player") == PlayerActor.Instance.transform)
+        //    {
+        //        enemy.chaseTarget = boundsHit.transform;
+        //        ToChaseState();
+        //    }
+        //enemy.gameObject.layer = oldlayer;
+
         //-----------------------------------------------------
-        //RaycastHit hit;
+        RaycastHit hit;
 
-        //if (Physics.Raycast(enemy.eyes.transform.position, enemy.eyes.transform.forward, out hit, enemy.sightRange) &&
-        //    (hit.collider.CompareTag("Player"))) //Eventuell CompareTag entfernen falls Layer angepasst werden
-        //{
-        //    enemy.chaseTarget = hit.transform;
-        //    ToChaseState();
-        //}
+        if ((Physics.Raycast(enemy.eyes.transform.position, enemy.eyes.transform.forward, out hit, enemy.sightRange) &&
+            (hit.collider.CompareTag("Player")))) //Eventuell CompareTag entfernen falls Layer angepasst werden
+        {
+            enemy.chaseTarget = hit.transform;
+            ToChaseState();
+        }
     }
 
 
