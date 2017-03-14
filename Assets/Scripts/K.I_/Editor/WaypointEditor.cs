9e04baa5e8a19cc41c8850cd1dac00ed19ca25b4
biwero <biwero@uni-trier.de>
2017-03-14 11:48:52 +0100

    added waypoint system

---
+++ Assets/Scripts/K.I_/Editor/WaypointEditor.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Assets.Scripts.K.I_;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Waypoints))]
+public class WaypointEditor : Editor {
+
+
+    void OnSelectionChanged() {
+
+    }
+
+    void OnEnable()
+    {
+        SceneView.onSceneGUIDelegate += OnSceneGUI;
+    }
+
+    void OnDisable()
+    {
+        SceneView.onSceneGUIDelegate -= OnSceneGUI;
+    }
+
+    void OnSceneGUI(SceneView sceneView){
+
+        var waypoints = target as Waypoints;
+        var spoints = serializedObject.FindProperty("points");
+
+        for (var i = 0; i < spoints.arraySize; ++i) {
+            var spoint = spoints.GetArrayElementAtIndex(i);
+            var waypoint = spoint.vector3Value;
+
+            EditorGUI.BeginChangeCheck();
+            var pos = Handles.FreeMoveHandle(waypoint, Quaternion.identity, 0.25f, Vector3.one * 0.25f, Handles.RectangleCap);
+            Handles.Label(new Vector3(pos.x, 0.5f, pos.z), "Point " + i);
+
+            if (EditorGUI.EndChangeCheck()) {
+                pos.y = 0;
+                spoint.vector3Value = pos;
+                serializedObject.ApplyModifiedProperties();
+            }
+
+        }
+
+        foreach (var pair in waypoints.pairs) {
+            if(pair.First == pair.Second) continue;
+
+            try {
+                var first = waypoints.points[pair.First];
+                var second = waypoints.points[pair.Second];
+                Handles.DrawLine(first, second);
+
+                var dir = (second - first).normalized;
+                Handles.ArrowCap(0, second - dir*1.2f, Quaternion.LookRotation(second-first, Vector3.up), 1);
+            }
+            catch (IndexOutOfRangeException) {
+
+            }
+        }
+
+        serializedObject.ApplyModifiedProperties();
+        AssetDatabase.SaveAssets();
+
+    }
+
+    public override void OnInspectorGUI() {
+        var waypoints = target as Waypoints;
+
+        var spoints = serializedObject.FindProperty("points");
+        EditorGUILayout.PropertyField(spoints, true);
+        if (GUILayout.Button("add")) {
+            spoints.InsertArrayElementAtIndex(spoints.arraySize);
+        }
+
+        var spairs = serializedObject.FindProperty("pairs");
+
+        for (int i = 0; i < spairs.arraySize; ++i) {
+
+            var point = spairs.GetArrayElementAtIndex(i);
+
+            point.FindPropertyRelative("First").intValue = Mathf.Clamp(point.FindPropertyRelative("First").intValue, 0, spoints.arraySize-1);
+            point.FindPropertyRelative("Second").intValue = Mathf.Clamp(point.FindPropertyRelative("Second").intValue, 0, spoints.arraySize-1);
+        }
+
+        EditorGUILayout.PropertyField(spairs, true);
+
+
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
---
+++ Assets/Scripts/K.I_/Waypoints.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Assets.Scripts.K.I_ {
+
+    [System.Serializable]
+    public struct Pair {
+        public int First, Second;
+
+        public sealed class FirstSecondEqualityComparer : IEqualityComparer<Pair> {
+            public bool Equals(Pair x, Pair y) {
+                return x.First == y.First && x.Second == y.Second;
+            }
+
+            public int GetHashCode(Pair obj) {
+                unchecked {
+                    return (obj.First * 397) ^ obj.Second;
+                }
+            }
+        }
+    }
+
+
+    [CreateAssetMenu()]
+    public class Waypoints : ScriptableObject {
+
+        public Vector3[] points;
+        public Pair[] pairs;
+
+
+        public Vector3 GetNextPoint(int currentIndex, out int nextIndex) {
+            var sel = pairs.Where(pairs => pairs.First == currentIndex).ToArray();
+
+            int i = Random.Range(0, sel.Length);
+
+            nextIndex = sel[i].Second;
+            return points[nextIndex];
+        }
+
+
+    }
+}
