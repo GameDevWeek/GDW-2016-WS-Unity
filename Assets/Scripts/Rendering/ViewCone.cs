dabbb6b5c7974d70693ac85520acf1a1383eb934
Armin Willer <armin_willer@yahoo.de>
2017-03-14 22:02:43 +0100

    Added different cone shader features

--- Assets/Scripts/Rendering/ViewCone.cs
+++ Assets/Scripts/Rendering/ViewCone.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
@@ -22,15 +23,18 @@ public class ViewCone : MonoBehaviour {
 
     private Vector3[] vertices;
     private Vector3[] normals;
+    private Vector2[] uv;
 
     private void Start()
     {
         vertices = new Vector3[nrOfRaycasts + 1];
         normals = new Vector3[nrOfRaycasts + 1];
+        uv = new Vector2[nrOfRaycasts + 1];
         mesh = new Mesh();
         mesh.name = transform.parent.name + " ViewConeMesh";
         meshRenderer = GetComponent<MeshRenderer>();
         meshFilter = GetComponent<MeshFilter>();
+        Camera.main.depthTextureMode = DepthTextureMode.DepthNormals;
     }
 
     private void Update()
@@ -44,16 +48,32 @@ public class ViewCone : MonoBehaviour {
         
         vertices[0] = Vector3.zero;
         normals[0] = Vector3.up;
+        uv[0] = Vector3.zero;
+
         RaycastHit hit;
-        for(int i = 0; i < nrOfRaycasts; ++i)
+        RaycastHit?[] hits = new RaycastHit?[nrOfRaycasts];
+
+        for (int i = 0; i < nrOfRaycasts; ++i)
         {
-            float angle = ((90 - (fieldOfView * 0.5f) + ((float)i / nrOfRaycasts) * fieldOfView)) % 360;
+            float angle = ((90 - (fieldOfView * 0.5f) + ((float) i / nrOfRaycasts) * fieldOfView)) % 360;
             Vector2 localDirection = MathUtility.DegreeToVector2(angle, 1);
             Vector3 globalDirection = transform.TransformDirection(new Vector3(localDirection.x, 0, localDirection.y));
 
             if (Physics.Raycast(transform.position, globalDirection, out hit, viewRadius, viewBlockingLayers))
             {
-                vertices[i + 1] = transform.InverseTransformPoint(hit.point);
+                hits[i] = hit;
+            }
+        }
+
+        for (int i = 0; i < nrOfRaycasts; ++i)
+        {
+            float angle = ((90 - (fieldOfView * 0.5f) + ((float)i / nrOfRaycasts) * fieldOfView)) % 360;
+            Vector2 localDirection = MathUtility.DegreeToVector2(angle, 1);
+            //Vector3 globalDirection = transform.TransformDirection(new Vector3(localDirection.x, 0, localDirection.y));
+
+            if (hits[i].HasValue)//Physics.Raycast(transform.position, globalDirection, out hit, viewRadius, viewBlockingLayers))
+            {
+                vertices[i + 1] = transform.InverseTransformPoint(hits[i].Value.point);
 
                 //Debug.DrawLine(transform.position, hit.point, Color.magenta);
             }
@@ -63,6 +83,19 @@ public class ViewCone : MonoBehaviour {
                 //Debug.DrawLine(transform.position, transform.position + new Vector3(direction2D.x * viewRadius, 0, direction2D.y * viewRadius), Color.magenta);
             }
             normals[i + 1] = Vector3.up;
+
+            if (i == 0)
+            {
+                uv[1] = Vector2.up;
+            }
+            else if (i == nrOfRaycasts - 1)
+            {
+                uv[nrOfRaycasts] = Vector2.one;
+            }
+            else
+            {
+                uv[i + 1] = new Vector2((float)i / nrOfRaycasts, 1.0f);
+            }
         }
         var triangles = new int[(nrOfRaycasts - 2) * 3 + 3];
         for (int i = 0, j = 0; i < triangles.Length; i += 3, ++j)
@@ -71,12 +104,13 @@ public class ViewCone : MonoBehaviour {
             triangles[i + 1] = j + 2;
             triangles[i + 2] = j + 1;
         }
-        triangles[triangles.Length - 3] = 0;
-        triangles[triangles.Length - 2] = 1;
-        triangles[triangles.Length - 1] = triangles.Length / 3 - 1;
+        //triangles[triangles.Length - 3] = 0;
+        //triangles[triangles.Length - 2] = 1;
+        //triangles[triangles.Length - 1] = triangles.Length / 3 - 1;
         mesh.vertices = vertices;
         mesh.normals = normals;
         mesh.triangles = triangles;
+        mesh.uv = uv;
         meshFilter.mesh = mesh;
     }
 }
--- Assets/Shaders/ViewConeShader.shader
+++ Assets/Shaders/ViewConeShader.shader
@@ -1,10 +1,9 @@
-﻿// Upgrade NOTE: replaced '_Object2World' with 'unity_ObjectToWorld'
-
-Shader "Rendering/ViewCone" {
+﻿Shader "Rendering/ViewCone" {
 
 	Properties
 	{
 		_Color("Main Color (A=Opacity)", Color) = (1,1,1,1)
+		_OutlineTex("Outline Texture", 2D) = "black" {}
 		_MinAlpha("Minimum Alpha", Float) = 0.2
 		_MaxAlpha("Maximum Alpha", Float) = 0.8
 		_LineThickness("Line Thickness", Float) = 0.5
@@ -13,37 +12,47 @@ Shader "Rendering/ViewCone" {
 
 	SubShader
 	{
-		Tags{ "Queue" = "Transparent" "IgnoreProjector" = "True" "RenderType" = "Transparent" }
-		LOD 100
-
+		//Blend One One
 		ZWrite Off
+		Cull Off
 		Blend SrcAlpha OneMinusSrcAlpha
+
+		Tags
+		{
+			"RenderType" = "Transparent"
+			"Queue" = "Transparent"
+		}
+		LOD 100
 
 		Pass
 		{
 			CGPROGRAM
-#pragma vertex vert
-#pragma fragment frag
-#pragma target 2.0
-#pragma multi_compile_fog
+			#pragma vertex vert
+			#pragma fragment frag
+			#pragma target 2.0
 
-#include "UnityCG.cginc"
+			#include "UnityCG.cginc"
 
 			struct appdata_t {
 				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
 			};
 
 			struct v2f {
 				float4 vertex : SV_POSITION;
 				float3 world_vertex : TEXCOORD0;
+				float2 screenuv : TEXCOORD1;
+				float depth : TEXCOORD2;
+				float2 uv : TEXCOORD3;
 			};
 
-			sampler2D _MainTex;
+			sampler2D _OutlineTex;
 			float4 _Color;
 			float _MinAlpha;
 			float _MaxAlpha;
 			float _LineThickness;
 			float _LineSpeed;
+			sampler2D _CameraDepthNormalsTexture;
 
 			float modulo(float a, float b) {
 
@@ -57,11 +66,24 @@ Shader "Rendering/ViewCone" {
 				v2f o;
 				o.vertex = UnityObjectToClipPos(v.vertex);
 				o.world_vertex = mul(unity_ObjectToWorld, v.vertex).xyz;
+				o.screenuv = ((o.vertex.xy / o.vertex.w) + 1) * 0.5;
+				o.screenuv.y = 1 - o.screenuv.y;
+				o.depth = -mul(UNITY_MATRIX_MV, v.vertex).z * _ProjectionParams.w;
+				o.uv = v.uv;
+
 				return o;
 			}
 
 			fixed4 frag(v2f i) : SV_Target
 			{
+				float screenDepth = DecodeFloatRG(tex2D(_CameraDepthNormalsTexture, i.screenuv).zw);
+				float diff = screenDepth - i.depth;
+				float intersect = 0;
+
+				if (diff > 0)
+					intersect = 1 - smoothstep(0, _ProjectionParams.w * 0.75, diff);
+				fixed4 intersectColor = fixed4(lerp(_Color.rgb, fixed3(1, 1, 1), pow(intersect, 4)), 1);
+
 				float pos = modulo((i.world_vertex.x + i.world_vertex.z) * _LineThickness + _Time[1] * _LineSpeed, 1.0);
 				float a;
 				if (pos < 0.25)
@@ -80,8 +102,11 @@ Shader "Rendering/ViewCone" {
 				{
 					a = _MinAlpha;
 				}
-				fixed4 col = _Color;
-				col.a *= a;
+
+				float3 outline = tex2D(_OutlineTex, i.uv).rgb;
+
+				fixed4 col = _Color /*+ intersectColor * intersect*/ + fixed4(outline,0);
+				col.a *= a + outline.r;
 				return col;
 			}
 			ENDCG
