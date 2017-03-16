8e834d62e13f11d5e94d768907c45aad6b4cd8c7
Thilo Bartel <bartelth@hochschule-trier.de>
2017-03-16 14:05:32 +0100

    InGameMenü im Prototype2 verfügbar

---
+++ Assets/Scripts/UI/LoadInGameMenuScene/AddLoad_IngamePaused.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/*Das InGameMenu wird als additive Scene mit Escape geladen
+ "bool pause" speichert den Pause-Status. Wird die Scene durch
+ Escape geladen, pausiert die andere Scene und pause wird auf
+ true gesetzt. Beim erneuten betätigen der Escape-Taste schließt
+ die InGameMenu-Scene wieder und die andere Scene wird fort-
+ gesetzt.
+ 
+ Dieses Script gehört in jede spielbare Levelscene!!!*/
+public class AddLoad_IngamePaused : MonoBehaviour {
+
+    private static bool pause = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!pause)
+            {
+                pausedOn();
+            }
+            else
+            {
+                pausedOff();
+            }
+        }
+	}
+
+    public static void setPause(bool p)
+    {
+        pause = p;
+    }
+
+    public void pausedOn()
+    {
+        Time.timeScale = 0;
+        SceneManager.LoadScene(2, LoadSceneMode.Additive);
+        pause = true;
+    }
+
+    public void pausedOff()
+    {
+        Time.timeScale = 1;
+        SceneManager.UnloadSceneAsync(2);
+        pause = false;
+    }
+}
