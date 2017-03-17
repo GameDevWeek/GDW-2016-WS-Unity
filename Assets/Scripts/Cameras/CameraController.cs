6be1833233665e4257b221da6ecad4b4c7895857
lowkey <mephistopheles@onlinehome.de>
2017-03-17 16:26:58 +0100

    spectator camera (to highlight gameplay elements)

---
+++ Assets/Scripts/Cameras/CameraController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(TopDownCamera), typeof(DeathCamera), typeof(SpectatorCamera))]
+public class CameraController : MonoBehaviour  {
+	
+	private TopDownCamera m_topDown;
+	private DeathCamera m_death;
+	private SpectatorCamera m_spectator;
+
+	private void Start() {
+		m_topDown = GetComponent<TopDownCamera> ();
+		m_death = GetComponent<DeathCamera> ();
+		m_spectator = GetComponent<SpectatorCamera> ();
+	}
+
+	public void EnableNormalCam() {
+		m_topDown.enabled = true;
+		m_death.enabled = false;
+		m_spectator.enabled = false;
+	}
+	public void EnableDeathCam() {
+		m_topDown.enabled = false;
+		m_death.enabled = true;
+		m_spectator.enabled = false;
+	}
+
+	public void EnableSpectatorCam(Transform target) {
+		m_topDown.enabled = false;
+		m_death.enabled = false;
+		m_spectator.SetTarget (target);
+		m_spectator.enabled = true;
+	}
+
+}
