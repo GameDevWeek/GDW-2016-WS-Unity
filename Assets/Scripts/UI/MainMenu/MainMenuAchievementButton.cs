fb8521e2f4c0f1490465883afed6806e284f690b
Thilo Bartel <bartelth@hochschule-trier.de>
2017-03-18 00:13:38 +0100

    Achievementbutton hinzugefügt

---
+++ Assets/Scripts/UI/MainMenu/MainMenuAchievementButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class MainMenuAchievementButton : MonoBehaviour {
+    public GameObject menuButtons;
+
+
+    public void clickedAchievements()
+    {
+        menuButtons.SetActive(false);
+    }
+}
