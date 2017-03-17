e2aa471485b0dbb248f86adf8da22f71b853533c
biwero <biwero@uni-trier.de>
2017-03-17 13:40:24 +0100

    Made level 1 playable, added tools

---
+++ Assets/Editor/ReplaceObjectTool.cs
@@ -0,0 +1,35 @@
+using UnityEditor;
+using UnityEngine;
+
+public class ReplaceObjectTool : EditorWindow {
+
+    [MenuItem("Tools/Utils/Object Replacer Tool")]
+    static void OpenWindow() {
+        EditorWindow.GetWindow<ReplaceObjectTool>();
+    }
+
+    private Object obj = null;
+
+    private void OnGUI() {
+
+        obj = EditorGUILayout.ObjectField("Prefab", obj, typeof(GameObject), false);
+
+        if (GUILayout.Button("Replace")) {
+            var go = obj as GameObject;
+            if(go == null) return;
+
+            var transforms = Selection.GetTransforms(SelectionMode.TopLevel);
+
+            foreach (var transform in transforms) {
+                var ng = PrefabUtility.InstantiatePrefab(go) as GameObject;
+                ng.transform.position = transform.position;
+                ng.transform.rotation = transform.rotation;
+                ng.transform.localScale = transform.localScale;
+                GameObject.DestroyImmediate(transform.gameObject);
+            }
+
+
+        }
+    }
+
+}
