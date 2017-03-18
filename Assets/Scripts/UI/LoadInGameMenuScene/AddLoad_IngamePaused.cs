382b3d603a03a34134f20c381fddfef6365b940a
Thilo Bartel <bartelth@hochschule-trier.de>
2017-03-18 16:49:25 +0100

    InGameMenu Schrift angepasst, Elefant im MainMenu verschoben

--- Assets/Scripts/UI/LoadInGameMenuScene/AddLoad_IngamePaused.cs
+++ Assets/Scripts/UI/LoadInGameMenuScene/AddLoad_IngamePaused.cs
@@ -1,14 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
-/*Das InGameMenu wird als additive Scene mit Escape geladen
- "bool pause" speichert den Pause-Status. Wird die Scene durch
- Escape geladen, pausiert die andere Scene und pause wird auf
- true gesetzt. Beim erneuten betätigen der Escape-Taste schließt
- die InGameMenu-Scene wieder und die andere Scene wird fort-
- gesetzt.
- 
- Dieses Script gehört in jede spielbare Levelscene!!!*/
+/*Dieses Script gehört in jede spielbare Levelscene!!!*/
 public class AddLoad_IngamePaused : MonoBehaviour {
 
     public string UISceneName;
