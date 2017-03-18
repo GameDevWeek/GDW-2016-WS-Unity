07920cfc07c46213f6bfee26f1eae6eda59c8837
Armin Willer <armin_willer@yahoo.de>
2017-03-18 14:04:33 +0100

    Outline Metal Shader and Pulsing pedestal outline

---
+++ Assets/Scripts/Rendering/Pulse.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pulse : MonoBehaviour {
+
+    [SerializeField]
+    private Vector3 pulse;
+
+    [SerializeField]
+    private float frequency;
+
+    private Vector3 startScale;
+
+    private void Start()
+    {
+        startScale = transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        float x = (Mathf.Sin(frequency * Time.time) * 0.5f + 1f) * pulse.x;
+        float y = (Mathf.Sin(frequency * Time.time) * 0.5f + 1f) * pulse.y;
+        float z =( Mathf.Sin(frequency * Time.time) * 0.5f + 1f) * pulse.z;
+        transform.localScale = startScale + new Vector3(x, y, z);
+	}
+}
