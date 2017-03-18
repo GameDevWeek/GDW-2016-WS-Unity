6e9140ae68e812779f6c169a47cb1699a0147469
biwero <biwero@uni-trier.de>
2017-03-18 10:45:55 +0100

    final menu integration

--- Assets/Scripts/UI/LoadInGameMenuScene/InGameMenu.cs
+++ Assets/Scripts/UI/LoadInGameMenuScene/InGameMenu.cs
@@ -1,18 +1,22 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 //Dieses Script gehört in die IngameMenuScene
 
 public class InGameMenu : MonoBehaviour {
 
     [SerializeField] private RectTransform PauseMenu;
+    [SerializeField] private Slider peanutCooldown;
+    [SerializeField] private Text peanutAmount;
+    [SerializeField] private Slider stealthDuration;
+    [SerializeField] private Slider wantedLevel;
+
+    [SerializeField] private float wantedLevel2 = 0.38f;
+    [SerializeField] private float wantedLevel3 = 0.72f;
+
+
 
     private bool paused;
-
-    // Use this for initialization
-    void Start()
-    {
-        //Input.GetButtonDown();
-    }
 
     // Update is called once per frame
     void Update()
@@ -28,6 +32,24 @@ public class InGameMenu : MonoBehaviour {
             PauseMenu.gameObject.SetActive(true);
             Time.timeScale = 0;
         }
+
+        if (WantedLevel.Instance.CurrentWantedStage == 0) {
+            wantedLevel.value = 0;
+        } else if (WantedLevel.Instance.CurrentWantedStage == 1) {
+            wantedLevel.value = Mathf.Lerp(0, wantedLevel2, WantedLevel.Instance.PercentCurrentWantedLevel);
+        }
+        else if (WantedLevel.Instance.CurrentWantedStage == 2) {
+            wantedLevel.value = Mathf.Lerp(wantedLevel2, wantedLevel3, WantedLevel.Instance.PercentCurrentWantedLevel);
+        }
+        else if (WantedLevel.Instance.CurrentWantedStage == 3) {
+            wantedLevel.value = 1;
+        }
+
+
+        peanutCooldown.value = PlayerActor.Instance.shootPeanuts.cooldown.progress;
+        peanutAmount.text = "x" + PlayerActor.Instance.shootPeanuts.ammo;
+
+        stealthDuration.value = PlayerActor.Instance.camouflageController.PercentTimeLeft;
 
     }
 
