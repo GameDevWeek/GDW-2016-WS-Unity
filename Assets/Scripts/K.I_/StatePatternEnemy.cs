71b5bc01b18b17d578d23156554aa2f572b4f0e5
Andreas Hergert <hergerta@hochschule-trier.de>
2017-03-18 12:52:47 +0100

    fix

--- Assets/Scripts/K.I_/StatePatternEnemy.cs
+++ Assets/Scripts/K.I_/StatePatternEnemy.cs
@@ -65,6 +65,7 @@ public class StatePatternEnemy : MonoBehaviour, INoiseListener {
         {
             enemyAnimator.SetFloat("BlendSpeed", (float) (navMeshAgent.velocity.magnitude/chaseSpeed));
         }
+        //Debug.Log(navMeshAgent.velocity.magnitude);
 
 
     }
