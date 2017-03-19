beaaafcce7be3f07fd853d64ef8c9982e3459c6e
Armin Willer <armin_willer@yahoo.de>
2017-03-19 12:58:02 +0100

    Added stun indicator to enemies

--- Assets/Scripts/Rendering/Pulse.cs
+++ Assets/Scripts/Rendering/Pulse.cs
@@ -10,6 +10,9 @@ public class Pulse : MonoBehaviour {
     [SerializeField]
     private float frequency;
 
+    [SerializeField]
+    private float timeOffset;
+
     private Vector3 startScale;
 
     private void Start()
@@ -20,9 +23,9 @@ public class Pulse : MonoBehaviour {
     // Update is called once per frame
     void Update ()
     {
-        float x = (Mathf.Sin(frequency * Time.time) * 0.5f + 1f) * pulse.x;
-        float y = (Mathf.Sin(frequency * Time.time) * 0.5f + 1f) * pulse.y;
-        float z =( Mathf.Sin(frequency * Time.time) * 0.5f + 1f) * pulse.z;
+        float x = (Mathf.Sin(frequency * Time.time + timeOffset) * 0.5f + 1f) * pulse.x;
+        float y = (Mathf.Sin(frequency * Time.time + timeOffset) * 0.5f + 1f) * pulse.y;
+        float z =( Mathf.Sin(frequency * Time.time + timeOffset) * 0.5f + 1f) * pulse.z;
         transform.localScale = startScale + new Vector3(x, y, z);
 	}
 }
